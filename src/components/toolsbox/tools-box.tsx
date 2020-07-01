1d11272fcd04ea34ed11aff9dd1ab8a723b9ebe2
dmitry <joydnby@gmail.com>
2020-07-01 16:45:23 +0300

    feat: basic toolbox template, redux integration

--- src/components/toolsbox/tools-box.tsx
+++ src/components/toolsbox/tools-box.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-
+import React, { MouseEvent } from 'react';
+import { connect } from 'react-redux';
+import { setActiveTool, setPrimaryColor } from '../../store/ac/tools';
+import { Action } from '../../store/action-types';
+import { colors } from '../../assets/data';
 import './tools-box.scss';
 
-function ToolsBox({ setActiveTool }): React.ReactElement {
+type Handler = (e: MouseEvent<HTMLButtonElement>) => Action;
+
+type Props = {
+  onSetActiveTool: (tool: number) => Action;
+  onSetPrimaryColor: (color: string) => Action;
+};
+
+function ToolsBox({
+  onSetActiveTool,
+  onSetPrimaryColor,
+}: Props): React.ReactElement {
+  const activeToolHandler: Handler = (e) =>
+    onSetActiveTool(Number(e.currentTarget.getAttribute('data-button')));
+
+  const colorPickerHandler: Handler = (e) =>
+    onSetPrimaryColor(String(e.currentTarget.getAttribute('data-button')));
+
   return (
     <div className="tools-box">
-      <button data-button={0}> pen </button>
-      <button data-button={1}> stroke </button>
+      <button data-button={0} onClick={activeToolHandler}>
+        pen
+      </button>
+      <button data-button={1} onClick={activeToolHandler}>
+        stroke
+      </button>
+
+      <button data-button={colors[1]} onClick={colorPickerHandler}>
+        black
+      </button>
+      <button data-button={colors[2]} onClick={colorPickerHandler}>
+        white
+      </button>
+      <button data-button={colors[3]} onClick={colorPickerHandler}>
+        red
+      </button>
     </div>
   );
 }
 
-export default ToolsBox;
+export default connect(null, (dispatch) => ({
+  onSetActiveTool: (tool: number) => dispatch(setActiveTool(tool)),
+  onSetPrimaryColor: (color: string) => dispatch(setPrimaryColor(color)),
+}))(ToolsBox);
